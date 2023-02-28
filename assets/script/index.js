0e5cd82001bf134cf87e7fe4759a7e3c68103e16
tamanchichan <tamanchichan@gmail.com>
2023-02-28 12:57:15 -0600

    function created

---
+++ assets/script/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const input = document.querySelector('.input-number');
+const guessCount = document.querySelector('.guess-count');
+const guess = document.querySelector('.guess-btn');
+const message = document.querySelector('.message');
+
+//  function randomNumber() {
+//   return Math.floor(Math.random() * 50);
+// }
+
+let random = Math.floor(Math.random() * 50);
+let guesses = 0;
+guessCount.innerText = `Guess: ${guesses}`;
+
+guess.addEventListener('click', function() {
+  let number = input.value;
+  
+  if (number == random) {
+    message.innerText = 'Congratulations! You guessed the right number.'
+  }
+  if (number > random) {
+    message.innerText = 'The number is lower';
+  }
+  if (number < random) {
+    message.innerText = 'The number is higher';
+  }
+  guesses++;
+  guessCount.innerText = `Guess: ${guesses}`;
+  input.value = '';
+  input.focus();
+})
+
+console.log(random);
